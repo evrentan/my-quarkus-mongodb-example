e66325c6efb7576bb8d3d8f2034bb9aac172f124
evren <evrentan@gmail.com>
2021-03-28 22:28:58 +0300

    Update EnterpriseEntity.java

--- src/main/java/com/tan/example/entity/EnterpriseEntity.java
+++ src/main/java/com/tan/example/entity/EnterpriseEntity.java
@@ -5,7 +5,6 @@ import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Random;
 
 @MongoEntity(collection = "enterprise")
 public class EnterpriseEntity extends PanacheMongoEntity {
@@ -14,13 +13,6 @@ public class EnterpriseEntity extends PanacheMongoEntity {
   @Size(min = 3, max = 50)
   public String name;
   public String webPage;
-
-  public static EnterpriseEntity findRandom() {
-    final long enterpriseCount = EnterpriseEntity.count();
-    Random random = new Random();
-    int randomHero = random.nextInt((int) enterpriseCount);
-    return EnterpriseEntity.findAll().page(randomHero, 1).firstResult();
-  }
 
   @Override
   public String toString() {
